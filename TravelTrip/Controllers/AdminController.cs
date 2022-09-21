71c28b5a137f20da59851a0beba5531e8baf8f89
gamzeatik <gamzeatik33@hotmail.com>
2022-09-21 20:29:41 +0300

    asp.net mvc

---
+++ TravelTrip/Controllers/AdminController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTrip.Models.Siniflar;
+
+namespace TravelTrip.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        Context c = new Context();
+        [Authorize]
+        public ActionResult Index()
+        {
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
+        }
+        [Authorize]
+        [HttpGet]
+        public ActionResult YeniBlog()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniBlog(Blog p)
+        {
+            c.Blogs.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult BlogSil(int id)
+        {
+            var b = c.Blogs.Find(id);
+            c.Blogs.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [Authorize]
+        public ActionResult BlogGetir(int id)
+        {
+            var bl = c.Blogs.Find(id);
+            return View("BlogGetir",bl);
+        }
+        public ActionResult BlogGuncelle(Blog b)
+        {
+            var blg = c.Blogs.Find(b.ID);
+            blg.Aciklama = b.Aciklama;
+            blg.Baslik = b.Baslik;
+            blg.BlogImage = b.BlogImage;
+            blg.Tarih = b.Tarih;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [Authorize]
+        public ActionResult YorumListesi()
+        {
+            var yorumlar = c.Yorumlars.ToList();
+            return View(yorumlar);
+        }
+        public ActionResult YorumSil(int id)
+        {
+            var b = c.Yorumlars.Find(id);
+            c.Yorumlars.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi");
+        }
+        [Authorize]
+        public ActionResult YorumGetir(int id)
+        {
+            var y = c.Yorumlars.Find(id);
+            return View("YorumGetir",y);
+        }
+        public ActionResult YorumGuncelle(Yorumlar y)
+        {
+            var yorum = c.Yorumlars.Find(y.ID);
+            yorum.KullaniciAdi = y.KullaniciAdi;
+            yorum.Mail = y.Mail;
+            yorum.Yorum = y.Yorum;
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi");
+        }
+        public ActionResult HakkimizdaListesi()
+        {
+            var hkk = c.Hakkimizdas.ToList();
+            return View(hkk);
+        }
+        public ActionResult Hakkimizda(int id)
+        {
+            var hakkimizda = c.Hakkimizdas.Find(id);
+            return View("Hakkimizda",hakkimizda);
+        }
+        public ActionResult HakkimizdaGuncelle(Hakkimizda h)
+        {
+            var hk = c.Hakkimizdas.Find(h.ID);
+            hk.FotoUrl = h.FotoUrl;
+            hk.Aciklama = h.Aciklama;
+            c.SaveChanges();
+            return RedirectToAction("HakkimizdaListesi");
+        }
+    }
+}
---
+++ TravelTrip/Controllers/ContactController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTrip.Models.Siniflar;
+
+namespace TravelTrip.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        Context c = new Context();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }        
+       
+        [HttpPost]
+        public ActionResult Index(Iletisim i)
+        {
+            c.Iletisims.Add(i);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
